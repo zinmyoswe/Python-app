48eaf2bd73ba16b3bb33baed87fde3812e9b6bc7
ZIN MYO <overmidnight12@gmail.com>
2019-06-19 20:39:07 -0700

    my changes

---
+++ app2.py
@@ -0,0 +1,8 @@
+name = "Thura"
+age = "20"
+lang = "python"
+
+print("Hi, i am developer"+name+",")
+print("i am early " +age+",")
+print("I learn many programming languages")
+print("I am dedicate in "+lang+".")
