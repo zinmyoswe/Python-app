bdb9ec553a10a9093f2bb1131c9d27eae51ed576
ZIN MYO <overmidnight12@gmail.com>
2019-06-19 21:45:46 -0700

    my changes

---
+++ app3.py
@@ -0,0 +1,15 @@
+from math import *
+
+phrase = "Thura Aung"
+print(phrase.upper())
+print(phrase.lower())
+
+my_num = 5
+
+print(str(my_num) + " my favourite no")
+print (sqrt(36))
+
+
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+print("Hello " +name +"!! and your age is "+age)
---
+++ app4.py
@@ -0,0 +1,5 @@
+no1 = input("Enter a number: ")
+no2 = input("Enter an another number: ")
+result = float(no1) + float(no2)
+
+print(result)
