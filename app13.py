b8711120166013f65e3e1a578e2c2a965d11b395
ZIN MYO <overmidnight12@gmail.com>
2019-06-20 22:28:46 -0700

    my changes

---
+++ app13.py
@@ -0,0 +1,12 @@
+for letter in "Thura Aung":
+    print(letter)
+
+friends = ["Nanlay","Maymyatnoe","QQMhwe"]
+for friend in friends:
+    print(friend)
+
+for index in range(5):
+    if index == 0:
+        print("first Iteration")
+    else:
+        print("Not first")
